2cdd475415599d694a305c83d1ee50bbedc07f05
Phạm Tuấn Vương <phamtuanvuong.dev@gmail.com>
2024-12-18 18:45:15 +0700

    push code

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -19,4 +19,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findById(long id); // null
 
     boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
